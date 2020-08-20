5cfec8dfec463b260dcf42944c3bd2b8d27e0e00
Medjahdi Lamia <medj.lam@gmail.com>
2020-08-20 14:23:38 +0100

    migration of info,stats,communes tables

---
+++ database/migrations/2020_08_20_112234_create_stats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('nbrmal');
+            $table->integer('nbrgue');
+            $table->integer('nbrmort');
+            $table->date('date');
+            $table->unsignedInteger('wilaya_id');
+            $table->foreign('wilaya_id')->references('id')->on('wilayas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stats');
+    }
+}
